e284a4857aedfaf8b5d439ed05b7f8885d4dd039
Halcyon Skinner <hdskinner@gmail.com>
2025-03-07 18:08:17 -0500

    Restructured code & Added Layouts

---
+++ personal-website/src/components/Layout.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header";
+import Footer from './Footer';
+
+interface LayoutProps {
+    children: React.ReactNode;
+  }
+  
+  const Layout: React.FC<LayoutProps> = ({ children }) => {
+    return (
+      <>
+        <Header />
+        {children}
+        <Footer />
+      </>
+    );
+  };
+
+export default Layout;
