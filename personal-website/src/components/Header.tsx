fba881ed61ee8b067bcaddfff076abeb01b665b3
Halcyon Skinner <hdskinner@gmail.com>
2025-03-07 21:51:56 -0500

    Added nicely aligned effects to header

--- personal-website/src/components/Header.tsx
+++ personal-website/src/components/Header.tsx
@@ -1,21 +1,22 @@
 import React, { useState, useEffect } from 'react';
 // NavLink is a speical type of Link object that is called by the ReactRouter
 import { NavLink } from "react-router-dom"
+import "./Header.css"
 
 const Header: React.FC = () => {
   // tracks hover status 
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <header className="sticky top-0 w-full bg-[#262626] text-white px-6 py-0 flex justify-between items-left">
+    <header className="sticky top-5 w-full bg-[#262626] text-white px-6 py-0 flex justify-between items-left">
       
   
-      <nav className="flex space-x-8">
+      <nav className="navbar flex space-x-8">
         <NavLink
           to = "/" 
             className={({ isActive }) =>
-              isActive ? "text-x1 font-bold text-white px-6 py-1"
-                       : "text-sm text-gray-400 px-6 py-1 hover:text-lg"
+              isActive ? "text-x1 font-bold text-white px-6 py-1 transition transform duration-200"
+                       : "text-sm text-gray-400 px-6 py-1 hover:scale-115 transition transform duration-200"
           }>
           halcy.dev
         </NavLink>
@@ -23,8 +24,8 @@ const Header: React.FC = () => {
         <NavLink
           to = "/about"
            className={({ isActive }) => 
-            isActive ? "text-x1 font-bold text-white px-6 py-1"
-                     : "text-sm text-gray-400 px-6 py-1 hover:text-lg"
+            isActive ? "text-x1 font-bold text-white px-6 py-1 transition transform duration-200"
+                     : "text-sm text-gray-400 px-6 py-1 hover:scale-115 transition transform duration-200"
           }>
           About 
         </NavLink>
@@ -32,8 +33,8 @@ const Header: React.FC = () => {
         <NavLink
           to = "/projcts"
             className={({ isActive }) => 
-              isActive ? "text-x1 font-bold text-white px-6 py-1"
-                        : "text-sm text-gray-400 px-6 py-1 hover:text-lg"
+              isActive ? "text-x1 font-bold text-white px-6 py-1 transition transform duration-200"
+                        : "text-sm text-gray-400 px-6 py-1 hover:scale-115 transition transform duration-200"
             }>
             Projects
           </NavLink>
@@ -41,8 +42,8 @@ const Header: React.FC = () => {
           <NavLink
           to = "/blog"
             className={({ isActive }) => 
-              isActive ? "text-x1 font-bold text-white px-6 py-1"
-                        : "text-sm text-gray-400 px-6 py-1 hover:text-lg"
+              isActive ? "text-x1 font-bold text-white px-6 py-1 transition transform duration-200"
+                        : "text-sm text-gray-400 px-6 py-1 hover:scale-115 transition transform duration-200"
             }>
             Blog
           </NavLink>
