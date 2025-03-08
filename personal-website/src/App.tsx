86dc0392f2f8ba9026c3e995bdae18b5d978df70
Halcyon Skinner <hdskinner@gmail.com>
2025-03-08 18:59:01 -0500

    Fixed routing

--- personal-website/src/App.tsx
+++ personal-website/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useState } from 'react'
 import Layout from './components/Layout';
+import Home from "./pages/Home"
 import './App.css'
 
 
@@ -9,8 +10,7 @@ const App: React.FC = () => (
     <BrowserRouter> 
       <Layout>
         <Routes>
-          <Route>
-          </Route>
+          <Route path="/" element={<Home />} />
         </Routes>
       </Layout>
     </BrowserRouter>
