53fb9c081034ec94f66da11237f11349f67faa81
Halcyon Skinner <hdskinner@gmail.com>
2025-03-08 18:59:35 -0500

    Added temporary profile picture

--- personal-website/src/pages/Home.tsx
+++ personal-website/src/pages/Home.tsx
@@ -1,4 +1,13 @@
+import React from "react";
+import profilePicture from "../assets/temp_picture.jpg"
+
 const Home: React.FC = () => {
-    return()
-   
+    return (
+      <div className="flex flex-col items-center">
+      <img src={profilePicture} alt="temp" />
+      <p className="text-xl mt-8 text-left">Hi! My name is Halcyon Skinner</p>  
+    </div>
+);
 }
+
+  export default Home
