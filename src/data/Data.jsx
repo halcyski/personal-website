0740f5f3b5950b92cdf7d684ba70a08c09b5c268
halcyski <hdskinner@gmail.com>
2026-03-31 22:33:03 -0400

    updated Data.jsx to reflect position & projects

--- src/data/Data.jsx
+++ src/data/Data.jsx
@@ -2,16 +2,16 @@
 
 export const personalInfo = {
     user: "halcyski",
-    host: "neu-portfolio",
+    host: "portfolio",
     name: "Halcyon Skinner",
     university: "Northeastern University",
     major: "Computer Science -- Concentration in Systems",
     minor: "Computer Engineering",
-    status: "Actively seeking Co-op/Internship opportunities",
+    status: "Co-Op @ STR",
     contact: {
         email: "skinner.h@northeastern.edu",
         github: "https://github.com/halcyski",
-        linkedin: "https://www.linkedin.com/in/halcyon-skinner-ab5482158/",
+        linkedin: "https://www.linkedin.com/in/halcy",
     }
 };
 
@@ -22,5 +22,14 @@ export const projectsData = [
         name: "Digital-CPU",
         description: "Single-Cycle CPU with custom instruction set, 4x32 register file, ALU; Turing Complete",
         link: "https://github.com/halcyski/digital-cpu",
-    }
+    },
+    {
+        permissions: "-rwxr-xr-x",
+        owner: personalInfo.user,
+        name: "Ring 0 Linux Kernel Anti-Cheat"
+        description: "Kerenel Level Anti-Cheat for Linux systems, developed on WSL2 and Linux natively"
+        link: "#"
+    }, 
+
+
 ];
